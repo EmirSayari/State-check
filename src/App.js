92fe394e2d4e5e6242fa02c14e3d9beda3893f3c
emir sayari <sayariamir2005@gmail.com>
2022-01-10 14:25:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from "react";
+import './App.css';
+import Person from './Person';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Person></Person>
+      </header>
+    </div>
+  );
+}
+export default App;
---
+++ src/Person.js
@@ -0,0 +1,39 @@
+import React from "react";
+class Person extends React.Component {
+  state = {
+    Person: {
+      fullName: "Emir",
+      bio: "bio",
+      
+      profession: "test"
+    },
+    show: true
+  };
+
+  handleShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show
+    });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.show && (
+          <>
+            <h1>{this.state.Person.fullName}</h1>
+            <h1>{this.state.Person.bio}</h1>
+            
+            <h1>{this.state.Person.profession}</h1>
+            <br></br>
+          </>
+        )}
+
+        <button onClick={this.handleShowPerson}>click here</button>
+      </>
+    );
+  }
+
+};
+export default Person;
